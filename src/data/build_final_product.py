4d2ca5071d7c29c5dc1afc3eaa57cf2543c4b42c
Daniel Lusk <daniel.lusk@posteo.net>
2024-10-23 08:50:05 +0000

    feat: add data packing/quantization to final products; save final geotiffs using rio-cogeo cog generation; add syscfg for final maps

--- src/data/build_final_product.py
+++ src/data/build_final_product.py
@@ -2,6 +2,8 @@
 and uploads to a target destination for sharing."""
 
 import json
+import shutil
+import subprocess
 import tempfile
 from datetime import datetime
 from pathlib import Path
@@ -10,14 +12,12 @@ import numpy as np
 import rasterio
 from box import ConfigBox
 from dask import compute, delayed
-from dask.distributed import Client
-from rasterio.enums import Resampling
-from rasterio.shutil import copy
 from scipy.spatial import KDTree
 
 from src.conf.conf import get_config
-from src.conf.environment import log
+from src.conf.environment import detect_system, log
 from src.io.upload_sftp import upload_file_sftp
+from src.utils.dask_utils import close_dask, init_dask
 from src.utils.dataset_utils import (
     get_model_performance,
     get_predict_dir,
@@ -26,12 +26,23 @@ from src.utils.dataset_utils import (
 from src.utils.trait_utils import get_trait_number_from_id
 
 
-def interpolate_like(decimated: np.ndarray, reference: np.ndarray) -> np.ndarray:
-    """Interpolate a decimated raster to match the extent of a reference."""
+def interpolate_like(
+    decimated: np.ndarray, reference_valid_mask: np.ndarray
+) -> np.ndarray:
+    """
+    Interpolate a decimated raster to match the extent of a reference.
+
+    Parameters:
+    decimated (np.ndarray): The decimated raster array where values are either 1, 0, or NaN.
+    reference_valid_mask (np.ndarray): A boolean mask indicating valid reference locations.
+
+    Returns:
+    np.ndarray: The interpolated raster array with the same shape as the input, where NaN values
+                in the decimated array are filled based on the nearest valid values.
+    """
     # ensure all values that are not equal to 1 or 0 are set to nan
     decimated = np.where((decimated == 1) | (decimated == 0), decimated, np.nan)
     nan_mask = np.isnan(decimated)
-    reference_valid_mask = ~np.isnan(reference)
     interpolation_mask = nan_mask & reference_valid_mask
 
     x_coords, y_coords = np.meshgrid(
@@ -57,21 +68,79 @@ def interpolate_like(decimated: np.ndarray, reference: np.ndarray) -> np.ndarray
     return filled_raster
 
 
-def pack_data(data: np.ndarray) -> tuple[float, float, int, np.ndarray]:
-    """Determine the scale and offset of data and return downcast data."""
+def pack_data(
+    data: np.ndarray, nbits: int = 16, signed: bool = True
+) -> tuple[np.float64, np.float64, int, np.ndarray]:
+    """
+    Packs the given data into a specified integer format with optional scaling and offset.
+    Parameters:
+    -----------
+    data : np.ndarray
+        The input data array to be packed.
+    nbits : int, optional
+        The number of bits for the integer representation (default is 16).
+    signed : bool, optional
+        Whether to use a signed integer type (default is True).
+    Returns:
+    --------
+    tuple[np.float64, np.float64, int, np.ndarray]
+        A tuple containing:
+        - scale (np.float64): The scale factor used for packing.
+        - offset (np.float64): The offset value used for packing.
+        - nodata_value (int): The value used to represent missing data.
+        - data_int16 (np.ndarray): The packed data array in the specified integer format.
+    Raises:
+    -------
+    FloatingPointError
+        If there is an invalid floating point operation during the packing process.
+    Notes:
+    ------
+    This function scales and offsets the input data to fit into the specified integer format.
+    It handles NaN values by replacing them with a designated nodata value.
+    """
+
     data_min = np.nanmin(data)
     data_max = np.nanmax(data)
+    dtype = f"int{nbits}" if signed else f"uint{nbits}"
 
-    nodata_value = -32768
-    bits = 16
+    nodata_value: int = -(2 ** (nbits - 1)) if signed else 0
+    scale = (data_max - data_min) / (2**nbits - 2)
+    scale = np.float64(scale)
+    offset = (data_max + data_min) / 2 if signed else data_min - scale
+    offset = np.float64(offset)
 
-    scale = (data_max - data_min) / (2**bits - 2)
-    offset = (data_max + data_min) / 2
-    data_int16 = np.where(
-        np.isnan(data),
-        nodata_value,
-        np.subtract(np.divide(data, scale), offset).round().astype("int16"),
-    )
+    np.seterr(invalid="raise")
+    try:
+        data_int16 = np.where(
+            np.isnan(data), nodata_value, np.round((data - offset) / scale)
+        ).astype(dtype)
+    except FloatingPointError as e:
+        log.error("FloatingPointError: %s", e)
+        log.error("data_min: %s", data_min)
+        log.error("data_max: %s", data_max)
+        log.error("scale: %s", scale)
+        log.error("offset: %s", offset)
+
+        offset_data = np.subtract(data, offset)
+        log.error("offset_data stats:")
+        log.error("min: %s", np.nanmin(offset_data))
+        log.error("max: %s", np.nanmax(offset_data))
+
+        scaled_data = np.divide(offset_data, scale)
+        log.error("scaled data stats:")
+        log.error("min: %s", np.nanmin(scaled_data))
+        log.error("max: %s", np.nanmax(scaled_data))
+
+        rounded_data = scaled_data.round()
+        log.error("rounded_data stats:")
+        log.error("min: %s", np.nanmin(rounded_data))
+        log.error("max: %s", np.nanmax(rounded_data))
+
+        cast_data = rounded_data.astype(dtype)
+        log.error("cast_data stats:")
+        log.error("min: %s", np.nanmin(cast_data))
+        log.error("max: %s", np.nanmax(cast_data))
+        raise
 
     return scale, offset, nodata_value, data_int16
 
@@ -89,8 +158,8 @@ def process_single_trait_map(
     """Process a single trait map."""
     log.info("Processing %s", trait_map)
 
-    if destination not in ["sftp", "local"]:
-        raise ValueError("Invalid destination. Must be one of 'sftp', 'local'.")
+    if destination not in ["sftp", "local", "both"]:
+        raise ValueError("Invalid destination. Must be one of 'sftp', 'local', 'both'.")
 
     trait_num = get_trait_number_from_id(trait_map.stem)
     trait_meta = trait_mapping[trait_num]
@@ -102,6 +171,8 @@ def process_single_trait_map(
     log.info("Reading and packing predict...")
     predict_path = trait_map / trait_set / f"{trait_map.stem}_{trait_set}_predict.tif"
     predict_ds = rasterio.open(predict_path, "r")
+    predict = predict_ds.read(1)
+    predict_valid_mask = ~np.isnan(predict)
 
     scales = []
     offsets = []
@@ -138,9 +209,12 @@ def process_single_trait_map(
     )
     aoa_dataset = rasterio.open(aoa_path, "r")
     log.info("Interpolating AoA...")
-    aoa = interpolate_like(aoa_dataset.read(2), predict)
+    aoa = interpolate_like(aoa_dataset.read(2), predict_valid_mask)
+
     log.info("Masking and downcasting AoA...")
     aoa = np.where(np.isnan(aoa), nodata, aoa).astype("int16")
+    scales.append(1)
+    offsets.append(0)
     ds_count += 1
 
     log.info("Gathering model performance metrics...")
@@ -178,11 +252,12 @@ def process_single_trait_map(
     cog_profile.update(
         driver="GTiff",
         tiled=True,
-        blockxsize=512,
-        blockysize=512,
+        blockxsize=256,
+        blockysize=256,
         compress="ZSTD",
         copy_src_overviews=True,
         interleave="band",
+        nodata=nodata,
     )
 
     new_tags = predict_ds.tags().copy()
@@ -209,11 +284,15 @@ def process_single_trait_map(
             new_dataset.update_tags(**new_tags)
 
             log.info("Setting scales and offsets...")
-            for i, (scale, offset) in enumerate(zip(scales, offsets)):
-                new_dataset.update_tags(i + 1, Scale=scale, Offset=offset)
-            new_dataset._set_all_scales(scales)
-            new_dataset._set_all_offsets(offsets)
+            scales = np.array(scales, dtype=np.float64)
+            offsets = np.array(offsets, dtype=np.float64)
+            new_dataset._set_all_scales(scales)  # pylint: disable=protected-access
+            new_dataset._set_all_offsets(offsets)  # pylint: disable=protected-access
 
+            for i in range(1, ds_count + 1):
+                new_dataset.update_tags(i, _FillValue=nodata)
+
+            log.info("Writing bands...")
             new_dataset.write(predict, 1)
             new_dataset.write(cov, 2)
             new_dataset.write(aoa, 3)
@@ -222,38 +301,43 @@ def process_single_trait_map(
                 1,
                 f"{trait_meta['short']} ({trait_agg[str(cfg.datasets.Y.trait_stat)]})",
             )
-            new_dataset.set_band_description(2, "Coefficient of variation")
+            new_dataset.set_band_description(2, "Coefficient of Variation")
             new_dataset.set_band_description(3, "Area of Applicability mask")
 
-            factors = [2, 4, 8, 16]
-
-            log.info("Building overviews...")
-            if predict_ds.overviews(1):
-                factors = predict_ds.overviews(1)
-
-            new_dataset.build_overviews(factors, Resampling.average)
-            new_dataset.update_tags(ns="rio_overview", resampling="average")
-
-        # append _cog to new_file_path
         cog_path = (
             new_file_path.parent / f"{new_file_path.stem}_cog{new_file_path.suffix}"
         )
-        copy(new_file_path, cog_path, driver="COG")
+        log.info("Writing COG...")
+        # Run command: rio cogeo create new_file_path cog_path
+        subprocess.run(
+            [
+                "rio",
+                "cogeo",
+                "create",
+                "--overview-resampling",
+                "average",
+                "--in-memory",
+                str(new_file_path),
+                str(cog_path),
+            ],
+            check=True,
+        )
 
-        if destination == "local":
-            dest_dir = get_processed_dir() / "final"
+        if destination in ("local", "both"):
+            dest_dir = get_processed_dir() / cfg.public.local_dir
             dest_dir.mkdir(parents=True, exist_ok=True)
+            # shutil move
+            shutil.move(cog_path, dest_dir / new_file_path.name)
+            # copy(cog_path, dest_dir / new_file_path.name, driver="COG")
 
-            copy(cog_path, dest_dir / new_file_path.name, driver="COG")
-
-        elif destination == "sftp":
+        if destination in ("sftp", "both"):
             # Upload the new file to the server
             log.info("Uploading to the server...")
             upload_file_sftp(
                 cog_path,
                 str(
                     Path(
-                        cfg.public.dir,
+                        cfg.public.sftp_dir,
                         cfg.PFT,
                         f"{cfg.model_res}deg",
                         new_file_path.name,
@@ -263,17 +347,17 @@ def process_single_trait_map(
 
         predict_ds.close()
         cov_dataset.close()
+        aoa_dataset.close()
 
 
 def main(cfg: ConfigBox = get_config()) -> None:
     """Main function."""
-    log.info("Adding metadata to predicted trait rasters...")
+    syscfg = cfg[detect_system()][cfg.model_res]["build_final_product"]
     predict_dir = get_predict_dir()
     trait_set: str = "splot_gbif"
 
     today = datetime.today().strftime("%Y-%m-%d")
 
-    # TODO: #10 Add full metadata @dluks
     metadata = {
         "author": "Daniel Lusk",
         "organization": "Department for Sensor-based Geoinformatics, University of Freiburg",
@@ -305,16 +389,21 @@ def main(cfg: ConfigBox = get_config()) -> None:
     with open("reference/trait_stat_mapping.json", "rb") as f:
         trait_stats = json.load(f)
 
-    with Client(
-        dashboard_address=cfg.dask_dashboard, n_workers=8, threads_per_worker=1
-    ):
-        tasks = [
-            process_single_trait_map(
-                trait_map, trait_set, cfg, metadata, trait_mapping, trait_stats
-            )
-            for trait_map in list(predict_dir.glob("*"))[:1]
-        ]
-        compute(*tasks)
+    client, cluster = init_dask(
+        dashboard_address=cfg.dask_dashboard,
+        n_workers=syscfg.n_workers,
+        threads_per_worker=syscfg.threads_per_worker,
+    )
+
+    tasks = [
+        process_single_trait_map(
+            trait_map, trait_set, cfg, metadata, trait_mapping, trait_stats, "both"
+        )
+        for trait_map in list(predict_dir.glob("*"))
+    ]
+    compute(*tasks)
+
+    close_dask(client, cluster)
 
     log.info("Done!")
 
