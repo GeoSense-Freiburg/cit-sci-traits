196ca9795579295501ba8826e47d5c48288013cf
Daniel Lusk <daniel.lusk@posteo.net>
2025-01-27 16:52:56 +0100

    add get_all_cov, get_all_aoa

--- src/utils/dataset_utils.py
+++ src/utils/dataset_utils.py
@@ -282,8 +282,7 @@ def get_trait_models_dir(trait: str, config: ConfigBox = cfg) -> Path:
 def get_all_trait_models(config: ConfigBox = cfg) -> Generator[Path, None, None]:
     """Get all trait models from each trait_set for a specific configuration."""
     for model_dir in get_models_dir().glob("X*"):
-        for ts_dir in get_latest_run(model_dir / config.train.arch).iterdir():
-            yield ts_dir
+        yield from get_latest_run(model_dir / config.train.arch).iterdir()
 
 
 def get_latest_run(runs_path: Path) -> Path:
@@ -363,6 +362,29 @@ def get_aoa_dir(config: ConfigBox = cfg) -> Path:
 def get_cov_dir(config: ConfigBox = cfg) -> Path:
     """Get the path to the cov directory for a specific configuration."""
     return get_processed_dir(config) / config.cov.dir
+
+
+def get_all_cov(config: ConfigBox = cfg) -> Generator[Path, None, None]:
+    """Get all cov maps for a given configuration."""
+    for trait_dir in get_cov_dir().glob("X*"):
+        yield from [
+            list(trait_set_dir.glob("*.tif"))[0]
+            for trait_set_dir in trait_dir.iterdir()
+        ]
+
+
+def get_all_aoa(config: ConfigBox = cfg) -> Generator[Path, None, None]:
+    """Get all cov maps for a given configuration."""
+    for trait_dir in get_aoa_dir().glob("X*"):
+        yield from [
+            list(trait_set_dir.glob("*.tif"))[0]
+            for trait_set_dir in trait_dir.iterdir()
+        ]
+
+
+def get_biome_map_fn(config: ConfigBox = cfg) -> Path:
+    """Get the path to the biome map file for a specific configuration."""
+    return Path(config.interim_dir, config.biomes.interim_path)
 
 
 def get_splot_corr_fn(config: ConfigBox = cfg) -> Path:
