51571347d0e7a39e522879edd6513d518cffe12a
Daniel Lusk <daniel.lusk@posteo.net>
2025-01-27 16:51:26 +0100

    add fn to plot multires splot correlation

--- src/visualization/model_assessment.py
+++ src/visualization/model_assessment.py
@@ -1,7 +1,13 @@
+from pathlib import Path
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from adjustText import adjust_text
+from matplotlib.axes import Axes
+
+from src.utils.trait_utils import get_trait_name_from_id
 
 
 def plot_observed_vs_predicted(
@@ -82,3 +88,146 @@ def plot_observed_vs_predicted(
     ax.set_title(name)
 
     return ax
+
+
+def plot_splot_correlations(
+    axes: list[Axes],
+    df: pd.DataFrame,
+    pft: str,
+    trait_set_ids: list[str],
+    unit: str = "km",
+    out_path: Path | None = None,
+):
+    """Plot sPlot correlations for GBIF and sPlot extrapolations for the given PFT"""
+    if len(axes) != len(trait_set_ids):
+        raise ValueError("The number of axes must be equal to the number of trait sets")
+
+    if unit not in ["km", "deg"]:
+        raise ValueError("unit must be either 'km' or 'deg'")
+    if unit == "km":
+        df = df.query(
+            f"pft == '{pft}' and transform == 'power' and resolution.str.endswith('km')"
+        )
+    else:
+        df = df.query(
+            f"pft == '{pft}' and transform == 'none' and resolution != '1km'"
+        )[["trait_id", "trait_set_id", "pearsonr", "resolution"]]
+
+    traits = df.trait_id.unique()
+
+    resolution_labels = ["1 km", "22 km", "55 km", "111 km"]
+    resolutions = ["1km", "22km", "55km", "111km"]
+    if unit == "deg":
+        resolution_labels = ["0.01", "0.2", "0.5", "1", "2"]
+        resolutions = ["001", "02", "05", "1", "2"]
+
+    # Figure directory
+    ncols = 1
+    if len(axes) > 1:
+        nrows = 1
+        ncols = len(trait_set_ids)
+        _, axes = plt.subplots(
+            nrows, ncols=ncols, figsize=(6.66 * ncols, 17 * nrows), dpi=300
+        )
+        if ncols > 1:
+            axes = axes.flatten()
+
+    Y_LIM = (0.1, 0.82)
+
+    for ax, trait_set_id in zip(axes, trait_set_ids):
+        text_x = 0.98
+
+        # Define colors
+        # colors = plt.cm.Paired(np.linspace(0, 1, len(stg.index.get_level_values(0).unique())))
+        # use sns instead (e.g. sns.hls_palette(h=.5))
+        colors = sns.color_palette(n_colors=len(traits), desat=0.75)
+
+        x_positions = [text_x] * len(traits)
+        y_positions = []
+        labels = []
+        label_colors = []
+
+        # Loop over each trait
+        for color, trait in zip(colors, traits):
+            # Select data for the current trait
+            trait_data = df.query(f"trait_id == '{trait}'")
+            trait_short = get_trait_name_from_id(trait)[0]
+
+            # Plot sPlot data with solid line and circular markers
+            # splot_data = trait_data.xs("sPlot", axis=0, level=1)
+            ts_data = trait_data.query(f"trait_set_id == '{trait_set_id}'")
+            y_positions.append(ts_data.pearsonr.values[-1])
+
+            ax.plot(
+                ts_data.resolution,
+                ts_data.pearsonr,
+                linestyle="-",
+                color=color,
+                label=f"{trait}",
+                marker="o",
+                markeredgecolor="white",
+                markeredgewidth=0.7,
+                ms=5,
+                linewidth=0.75,
+            )
+
+            labels.append(trait_short)
+            label_colors.append(color)
+
+        texts = []
+        for x_position, y_position, label, color in zip(
+            x_positions, y_positions, labels, label_colors
+        ):
+            text = ax.text(
+                x_position,
+                y_position,
+                label,
+                ha="right",
+                va="center",
+                color=color,
+                fontsize="x-small",
+            )
+            texts.append(text)
+
+        # make sure the plots share the same y-axis
+        if ncols > 1:
+            ax.set_ylim(*Y_LIM)
+
+        adjust_text_kwargs = {
+            "force_text": (0, 0.5),
+            "only_move": {"text": "y", "static": "y", "explode": "y", "pull": "y"},
+        }
+
+        adjust_text(
+            texts,
+            ax=ax,
+            **adjust_text_kwargs,
+        )
+
+        # Readjust the x-position of the text since adjust_text doesn't seem to respect the
+        # only_move parameter and still moves the text in the x-direction
+        def _reset_text_x(_texts, _x_position, _ax):
+            for _text in _texts:
+                _text.set_ha("left")
+                _text.set_x(_x_position)
+                _text.set_transform(_ax.get_yaxis_transform())
+
+        _reset_text_x(texts, text_x, ax)
+
+        ax.set_xticks(range(len(resolutions)), resolution_labels)
+        ax.set_xticklabels(resolution_labels)
+
+        ax.set_xlabel("Resolution ($\degree$)")
+        ax.set_ylabel("$r$")
+        # ax.invert_xaxis()
+
+        # Remove the gridlines
+        ax.grid(False)
+
+    # add space between plots
+    plt.subplots_adjust(wspace=0.5)
+
+    if out_path:
+        plt.savefig(out_path, dpi=300, bbox_inches="tight")
+
+    return axes
